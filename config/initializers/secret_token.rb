ecb4af6df623fcb78ba404dbd21a9851de361190
vincent <vincent@Ubuntu.(none)>
2013-02-14 17:41:07 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeamTracker::Application.config.secret_token = '605cd2dd54394f06fef03ffa0dd6502aeca85588a519b6627439c3817d6a4a596e1845865cc48022ca6b4d49d9af53cce805503ed6f775151a1818c12aa4d710'
